98e62313a940b9a819c53272da291b77a091e285
Pragati Chothe <pragatichothe@gmail.com>
2025-08-24 21:41:34 +0530

    ✅ Step 3 : Code Writing | Added Inventory class with every details and checked if it's working

---
+++ 2. Vending-Machine/Inventory.java
@@ -0,0 +1,85 @@
+import java.util.*;
+
+public class Inventory {
+    
+    Map<Integer, Integer> productQuantities; // productId -> quantity
+    Map<Integer, Product> productCatalog;    // productId -> product details
+
+    public Inventory() {
+        
+        productQuantities = new HashMap<>();
+        productCatalog = new HashMap<>();
+    }
+
+    // Add new Product in Inventory
+    public void addProduct(Product product, int quantity) {
+        
+        // Get Product Id from Product class
+        int productId = product.getId();
+
+        // Get Product Quantity so far
+        int prevProductQuantity = productQuantities.getOrDefault(productId, 0);
+
+        productCatalog.put(productId, product);
+        
+        productQuantities.put(productId, prevProductQuantity + quantity);
+
+        return;
+    }
+
+    // Check if product is available or not 
+    public boolean isAvailable(int productId) {
+        
+        // Get Product Quantity so far
+        int prevProductQuantity = productQuantities.getOrDefault(productId, 0);
+
+        if (prevProductQuantity > 0) {
+            
+            return true;
+        }
+
+        return false;
+    }
+
+
+    // Get that product details 
+    public Product getProduct(int productId) {
+        
+        return productCatalog.get(productId);
+    }
+
+
+    // After buying product we need to reduce quantity from our data 
+    public void reduceQuantity(int productId) {
+        
+        if (isAvailable(productId)) {
+            productQuantities.put(productId, productQuantities.get(productId) - 1);
+        }
+
+        return;
+    }
+
+
+    // Get product Quantity
+    public int getQuantity(int productId) {
+        
+        return productQuantities.getOrDefault(productId, 0);
+    }
+
+    // IF admin want to see full inventory details print it 
+    public void printInventory() {
+        
+        System.out.println("Inventory Details : ");
+        
+        for (Integer productId : productCatalog.keySet()) {
+            
+            Product product = productCatalog.get(productId);
+            int qty = getQuantity(productId);
+            
+            System.out.println("\t" + product.getName() + " (Rs." + product.getPrice() + ") -> " + qty + " left");
+        }
+
+        return;
+    }
+
+}
--- 2. Vending-Machine/VendingMachine.java
+++ 2. Vending-Machine/VendingMachine.java
@@ -2,19 +2,36 @@ public class VendingMachine {
     
     public static void main(String[] args) {
         
+        Inventory inventory = new Inventory();
+        
         Product coke = new Product(1, "Coke", 101,  40);
         Product pepsi = new Product(2, "Pepsi", 102, 30);
         Product chips = new Product(3, "Chips", 103, 20);
         Product water = new Product(4, "Water", 104, 10);
 
         // Print objects
-        System.out.println(coke);
-        System.out.println(pepsi);
-        System.out.println(chips);
-        System.out.println(water);
+        System.out.println("Product Details : ");
+        System.out.println("\t" + coke);
+        System.out.println("\t" + pepsi);
+        System.out.println("\t" + chips);
+        System.out.println("\t" + water);
 
         // Or check getters
-        System.out.println("Pepsi price = Rs." + pepsi.getPrice());
-        System.out.println("Water slot code = " + water.getCode());
+        System.out.println("    - Pepsi price = Rs." + pepsi.getPrice());
+        System.out.println("    - Water slot code = " + water.getCode());
+
+
+        // Add product in inventory
+        inventory.addProduct(coke, 5);
+        inventory.addProduct(pepsi, 3);
+        inventory.addProduct(chips, 7);
+
+        System.out.println("Products added in Inventory");
+
+        inventory.printInventory();
+
+        // Try reducing product stock from inventory
+        inventory.reduceQuantity(2);
+        System.out.println("    - Pepsi left: " + inventory.getQuantity(2));
     }
 }
