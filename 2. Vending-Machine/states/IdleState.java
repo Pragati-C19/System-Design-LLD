08e61757bcd0301d7b38f89fffee131efa0dacf7
Pragati Chothe <pragatichothe@gmail.com>
2025-08-25 19:25:23 +0530

    ✅ Step 3 : Code Writing | Was having issue for importing VendingMachine class in states so move VendingMachine.java File to states folder

---
+++ 2. Vending-Machine/states/IdleState.java
@@ -0,0 +1,25 @@
+package states;
+
+public class IdleState {
+
+    VendingMachine vendingMachine;
+
+    public IdleState(VendingMachine vendingMachine) {
+        
+        this.vendingMachine = vendingMachine;
+    }
+
+    public void insertCash(int amount) {
+        
+        System.out.println("Coin inserted: " + amount);
+    }
+
+    public void selectProduct(int productId) {
+        System.out.println("Insert money first before selecting a product.");
+    }
+
+    public void cancel() {
+        System.out.println("No money inserted. Nothing to cancel.");
+    }
+
+}
--- 2. Vending-Machine/states/VendingMachine.java
+++ 2. Vending-Machine/states/VendingMachine.java
@@ -1,3 +1,5 @@
+package states;
+
 public class VendingMachine {
     
     public void sayHello() {
